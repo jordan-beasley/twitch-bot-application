4e42de2ade016d8555d7020e0cf15c9a5c422c26
Jordan Beasley <jordan.beasley@smail.astate.edu>
2016-12-15 21:19:10 -0600

    Initial bot form

---
+++ botform/botform/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace botform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ContentPanel = new System.Windows.Forms.Panel();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.chatBox = new System.Windows.Forms.TextBox();
+            this.chatDisplay = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.ContentPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ContentPanel
+            // 
+            this.ContentPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ContentPanel.BackColor = System.Drawing.SystemColors.Control;
+            this.ContentPanel.Controls.Add(this.btnSubmit);
+            this.ContentPanel.Controls.Add(this.chatBox);
+            this.ContentPanel.Controls.Add(this.chatDisplay);
+            this.ContentPanel.Location = new System.Drawing.Point(12, 12);
+            this.ContentPanel.Name = "ContentPanel";
+            this.ContentPanel.Size = new System.Drawing.Size(579, 417);
+            this.ContentPanel.TabIndex = 0;
+            this.ContentPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.ContentPanel_Paint);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnSubmit.Location = new System.Drawing.Point(378, 375);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(92, 30);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // chatBox
+            // 
+            this.chatBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.chatBox.Location = new System.Drawing.Point(6, 317);
+            this.chatBox.Multiline = true;
+            this.chatBox.Name = "chatBox";
+            this.chatBox.Size = new System.Drawing.Size(366, 88);
+            this.chatBox.TabIndex = 1;
+            // 
+            // chatDisplay
+            // 
+            this.chatDisplay.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.chatDisplay.AutoSize = true;
+            this.chatDisplay.Location = new System.Drawing.Point(3, 14);
+            this.chatDisplay.Name = "chatDisplay";
+            this.chatDisplay.Size = new System.Drawing.Size(37, 17);
+            this.chatDisplay.TabIndex = 0;
+            this.chatDisplay.Text = "Chat";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 441);
+            this.Controls.Add(this.ContentPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ContentPanel.ResumeLayout(false);
+            this.ContentPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel ContentPanel;
+        private System.Windows.Forms.Label chatDisplay;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox chatBox;
+        private System.Windows.Forms.Button btnSubmit;
+    }
+}
+
---
+++ botform/botform/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+
+namespace botform
+{
+    public partial class Form1 : Form
+    {
+        TcpClient tcpClient;
+        StreamReader s_reader;
+        StreamWriter s_writer;
+
+        string username;
+        string password;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            ContentPanel.Dock = DockStyle.Fill;
+
+            if (getUserInfo())// try to get the users info from file
+            {
+
+                Connect();
+
+            }else // if it can't be found, load the filler page
+            {
+
+                ContentPanel.Controls.Add(SignIn.Instance);
+                SignIn.Instance.BringToFront();
+
+            }
+            
+        }
+
+        public bool getUserInfo()
+        {
+            bool retrieved = false;
+
+
+            return retrieved;
+        }
+
+        void Connect()
+        {
+
+            tcpClient = new TcpClient("irc.twitch.tv", 6667);
+            s_reader = new StreamReader(tcpClient.GetStream());
+            s_writer = new StreamWriter(tcpClient.GetStream());
+
+            //string username = "zklown";
+            //string password = File.ReadAllText("auth.txt");
+
+            s_writer.WriteLine("PASS " + password + Environment.NewLine
+                + "NICK " + username + Environment.NewLine
+                + "USER " + username + " 8 * :" + username);
+
+            s_writer.Flush();
+            s_writer.WriteLine("JOIN #zklown");
+            s_writer.Flush();
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (tcpClient != null && !tcpClient.Connected)
+            {
+                chatDisplay.Text += "\r\nAttempting to connect";
+                Connect();
+
+            }else
+            {
+                chatDisplay.Text += "\r\nCould not connect";
+                this.timer1.Enabled = false; // if we couldn't connect, stop the timer
+            }
+
+            if (tcpClient != null)
+            {
+
+                if (tcpClient.Available > 0 || s_reader.Peek() > 0)
+                {
+                    string message = s_reader.ReadLine();
+
+                    chatDisplay.Text += $"\r\n{message}";
+
+                }
+            }
+        }
+
+
+
+        private void ContentPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+
+
+        }
+
+        // when button is clicked to submit, check text then post reply in chat
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
